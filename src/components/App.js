475a3d7381d064ae1ce9ddfa46486ca68336c435
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2019-12-25 19:24:54 -0600

    done for now

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ class App extends React.Component {
     return (
       <div className='app-wrapper'>
         <div className='main-greet'>Feliz Navidad Isabela</div>
+        <div className='main-greet'>Te Quiero Mucho</div>
+        <div className='main-greet'>Hoy Y Siempre</div>
         <div className='app'>
           <SkyView backgroundColor={'#7328a8'} size={'75px'} how={50} />
           {/* pass frontStyle and backStyle props */}
